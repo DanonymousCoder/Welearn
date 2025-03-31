c0702da35b7b875360e073ae4ca49b2ce3c1085c
Ridwanullah <aladebaruwa@gmail.com>
2025-03-31 12:58:40 +0100

    minor changes

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let ham = document.getElementById("nav-toggler");
 let navClose = document.getElementById("nav-close");
 let navLinks = document.getElementById("nav-links");
+let link = document.getElementById("link");
 
 ham.addEventListener("click", showMobileNav);
 navClose.addEventListener("click", closeMobileNav);
+link.addEventListener("click", () => {
+    navLinks.classList.add("f-close");
+})
 
 function showMobileNav() {
     navLinks.classList.add("active");
