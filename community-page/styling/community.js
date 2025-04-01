59ead88df82455477905ac2c3816a741a955793e
X-CodeMaster <Samueltajayi245@gmail.com>
2025-04-01 09:37:56 +0100

    Update community.css

--- community-page/styling/community.js
+++ community-page/styling/community.js
@@ -2,5 +2,6 @@ const MenuButtons = document.getElementsByClassName("menu-btn");
 function toggleMenu(button) {
   if (button.nextElementSibling) {
     button.nextElementSibling.classList.toggle("show");
+    button.classList.toggle("rotate");
   }
 }
