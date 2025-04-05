12648a5e65d9ae4abeda54e2a48f6a568d292970
Ridwanullah <aladebaruwa@gmail.com>
2025-04-05 10:33:16 +0100

    minor change

---
+++ community.js
@@ -0,0 +1,17 @@
+const MenuButtons = document.getElementsByClassName("menu-btn");
+// const ProfileImage = document.getElementsByClassName("profile-img");
+
+function toggleMenu(button) {
+    if (button.nextElementSibling) {
+        button.nextElementSibling.classList.toggle("show");
+        button.classList.toggle("rotate");
+    }
+}
+
+// function UserActive() {
+//   if (profile.classList.contain(active)) {
+//     profile.classList.toggle("active");
+//   } else {
+//     profile.classList.remove("active");
+//   }
+// }
